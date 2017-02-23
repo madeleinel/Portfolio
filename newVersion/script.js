635d25dfa71e8285652cef702b3400017949e15d
madeleinel <madeleinelinder@hotmail.com>
2017-02-23 17:49:22 +0000

    added site mindmap & the base for the fourth version of the site

---
+++ newVersion/script.js
@@ -0,0 +1,9 @@
+// send message to my email when click sendEmail button
+
+const emailBtn = document.getElementById('sendEmail');
+
+emailBtn.addEventListener('click', (e) => {
+
+  // take content from form fields and transform it into an email
+
+})
