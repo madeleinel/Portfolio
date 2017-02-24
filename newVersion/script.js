6b0d6b89baa62882085f8fba7544ae9f6c9a9f97
madeleinel <madeleinelinder@hotmail.com>
2017-02-24 02:24:18 +0000

    updated the layout & added more functions, and added 2 placeholder images

--- newVersion/script.js
+++ newVersion/script.js
@@ -7,3 +7,75 @@ emailBtn.addEventListener('click', (e) => {
   // take content from form fields and transform it into an email
 
 })
+
+
+
+// Hide the header (apart from the nav bar) when scroll down the page, from the landing page onto the main part of the site
+
+// one idea: add and remove CSS classes from an element in response to a scroll event >> http://wicky.nillia.ms/headroom.js/
+
+// const header = document.getElementById('headerUnpinned');
+// const navBar = document.getElementById('headerPinned');
+// const nav = document.querySelectorAll('#headerPinned');
+//
+// window.addEventListener('scroll', (e) => {
+//
+//   if (e.srcElement.body.scrollTop < 100) {
+//     // full header should display
+//     console.log("under 100!")
+//     // nac.style.top =
+//   } else if (e.srcElement.body.scrollTop >= 100) {
+//     // only the nav bar should display
+//     console.log("over 100!");
+//   }
+//
+//   // header.style.display = "none";
+//   // navBar.style.position = "fixed";
+//   // navBar.style.top = 0;
+//   // navBar.style.left = 0;
+//   // console.log(e);
+//   // console.log(e.srcElement.body.scrollTop);
+//   console.log(nav);
+// });
+
+// console.log(header);
+// console.log(navBar);
+
+// then reverse when scroll back to top of page, so:
+// when e.srcElement.body.scrollTop === <= 100 (good value?)
+// display the full header again
+
+
+
+// Setting up portfolio slideshow
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("slides");
+  var dots = document.getElementsByClassName("chooseImage");
+
+  if (n > x.length) {
+    slideIndex = 1;
+  }  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace(" currentImage", "");
+  }
+  x[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " currentImage";
+}
