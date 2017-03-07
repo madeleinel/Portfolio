2716bc73d3899fca2d7cdbe512ba92319549388b
madeleinel <madeleinelinder@hotmail.com>
2017-03-07 19:38:39 +0000

    styled page footer & reorganised project files

--- script.js
+++ script.js
@@ -1,105 +1,34 @@
-// Setting up portfolio slideshow
+// Set up the portfolio slideshow
 
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
+let slideIndex = 1;
+showDiv(slideIndex);
 
 function currentDiv(n) {
-  showDivs(slideIndex = n);
+  showDiv(slideIndex = n);
 }
 
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("pSlides");
-  var dots = document.getElementsByClassName("chooseImage");
+function showDiv(n) {
+  let i;
+  const slides = document.getElementsByClassName("slides");
+  const numOfSlides = slides.length;
+  const options = document.getElementsByClassName("chooseImage");
+  const numOfOptions = options.length;
 
-  if (n > x.length) {
+  if (n > numOfSlides) {
     slideIndex = 1;
-  }  if (n < 1) {
-    slideIndex = x.length;
-  }
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-     dots[i].className = dots[i].className.replace(" currentImage", "");
+  } if (n < 1) {
+    slideIndex = numOfSlides;
   }
 
-// NEED TO UNCOMMENT THE 2 BELOW LINES TO MAKE THE SLIDESHOW WORK:
+  for (i = 0; i < numOfSlides; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < numOfOptions; i++) {
+    options[i].className = options[i].className.replace(" currentImage", "");
+  }
 
-  // x[slideIndex-1].style.display = "block";
-  // dots[slideIndex-1].className += " currentImage";
+  slides[slideIndex-1].style.display = "-webkit-flex";
+  slides[slideIndex-1].style.display = "-ms-flex";
+  slides[slideIndex-1].style.display = "flex";
+  options[slideIndex-1].className += " currentImage";
 }
-
-// Setting up modal pop-ups
-
-// Get the modal
-const aboutModal = document.getElementById("aboutModal");
-const skillsModal = document.getElementById("skillsModal");
-const experModal = document.getElementById("experModal");
-const portModal = document.getElementById("portModal");
-
-// Get the button that opens the modal
-const aboutBtn = document.getElementById("aboutBtn");
-const skillsBtn = document.getElementById("skillsBtn");
-const experBtn = document.getElementById("experBtn");
-const portBtn = document.getElementById("portBtn");
-
-// Get the <span> element that closes the modal
-// let closeBtn = document.querySelectorAll(".closeBtn")[0];
-const closeAbtBtn = document.getElementById("closeAbtBtn");
-const closeSksBtn = document.getElementById("closeSksBtn");
-const closeExpBtn = document.getElementById("closeExpBtn");
-const closePrtBtn = document.getElementById("closePrtBtn");
-
-// When the user clicks on the button, open the modal
-aboutBtn.onclick = function() {
-  aboutModal.style.display = "block";
-  skillsModal.style.display = "none";
-  experModal.style.display = "none";
-  portModal.style.display = "none";
-}
-skillsBtn.onclick = function() {
-  skillsModal.style.display = "block";
-  aboutModal.style.display = "none";
-  experModal.style.display = "none";
-  portModal.style.display = "none";
-}
-experBtn.onclick = function() {
-  experModal.style.display = "block";
-  aboutModal.style.display = "none";
-  skillsModal.style.display = "none";
-  portModal.style.display = "none";
-}
-portBtn.onclick = function() {
-  portModal.style.display = "block";
-  aboutModal.style.display = "none";
-  skillsModal.style.display = "none";
-  experModal.style.display = "none";
-}
-
-// When the user clicks on <span> (x), close the modal
-closeAbtBtn.onclick = function() {
-  aboutModal.style.display = "none";
-}
-closeSksBtn.onclick = function() {
-  skillsModal.style.display = "none";
-}
-closeExpBtn.onclick = function() {
-  experModal.style.display = "none";
-}
-closePrtBtn.onclick = function() {
-  portModal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//   if (event.target == aboutModal) {
-//     aboutModal.style.display = "none";
-//   } if (event.target == skillsModal) {
-//     skillsModal.style.display = "none";
-//   }
-// }
