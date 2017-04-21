0dc4fc2b270ababbc5b95da78966646e12996235
madeleinel <madeleinelinder@hotmail.com>
2017-04-21 19:48:56 +0100

    Creating gitignore file. Restructuring HTML/CSS files to one file per URL path. Reorganised ll static files to adhere to node folder structure

--- server.js
+++ server.js
@@ -4,11 +4,24 @@ var express = require('express');
 var app = express();
 app.use(express.static('public'));
 
-var formidable = require('express-formidable');
-app.use(formidable());
+// var formidable = require('express-formidable');
+// app.use(formidable());
 
+app.get('/', function (request, response) {
+  response.send('Landing page');
+});
 
+app.get('/about', function (request, response) {
+  response.sendFile(__dirname + '/public/about.html');
+});
 
+app.get('/portfolio', function (request, response) {
+  response.sendFile(__dirname + '/public/portfolio.html');
+});
+
+app.get('/contact', function (request, response) {
+  response.sendFile(__dirname + '/public/contact.html');
+});
 
 app.listen(5500, function() {
   console.log('Server is listening on port 5500. Ready to accept requests!')
