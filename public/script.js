cc4ce9fc5f6a8a74d0875707a40050edae127e59
madeleinel <madeleinelinder@hotmail.com>
2017-05-01 11:35:51 +0100

    Modifying the eventlistener in continued attempt to make the dropdown toggle icon work on touch screens. Also added an eventlistener to load all js code in page load.

--- public/script.js
+++ public/script.js
@@ -1,18 +1,22 @@
 // Set up dropdown menu for small screens
 
-const icon = document.getElementById("navToggleIcon");
-const navbar = document.getElementById("topNav");
+window.addEventListener("load", (active) => {   // Enable the below functions on page load
 
-["click", "touchstart"].forEach( input => {
-  icon.addEventListener(input, (e) => {
-    e.preventDefault()    // Prevent 'click' to be triggered as well when tapping on the icon using touch screens
-    if (navbar.id === "topNav") {
-      navbar.id += "_responsive";   // Change id of element to 'topNav_responsive'
-    } else {
-      navbar.id = "topNav";   // If id name is already 'topNav_responsive', change it back to 'topNav'
-    }
-  }, false);
-});
+  const icon = document.getElementById("navToggleIcon");
+  const navbar = document.getElementById("topNav");
+
+  ["touchstart"].forEach( input => {   // Could use touchstart only ; supposed to work on touch and click  >> to add at start of line: "click", 
+    icon.addEventListener(input, (e) => {
+      e.preventDefault()    // Prevent 'click' to be triggered as well when tapping on the icon using touch screens
+      if (navbar.id === "topNav") {
+        navbar.id += "_responsive";   // Change id of element to 'topNav_responsive'
+      } else {
+        navbar.id = "topNav";   // If id name is already 'topNav_responsive', change it back to 'topNav'
+      }
+    }, false);
+  });
+
+}, false);
 
 // icon.addEventListener("click", (e) => {
 //   if (navbar.id === "topNav") {
