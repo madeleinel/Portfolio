83995cb1b54f4086d0e01112d77954ef469a3a2e
madeleinel <madeleinelinder@hotmail.com>
2017-05-01 23:00:32 +0100

    Modifying JS code in further attempts to enable drowdown menu toggle icon on souch screens

--- public/script.js
+++ public/script.js
@@ -1,22 +1,26 @@
 // Set up dropdown menu for small screens
 
-window.addEventListener("load", () => {   // Enable the below functions on page load
+// window.addEventListener("load", () => {   // Enable the below functions on page load
 
   const icon = document.getElementById("navToggleIcon");
   const navbar = document.getElementById("topNav");
 
-  ["click", "touchstart"].forEach( input => {   // Could use touchstart only ; supposed to work on touch and click >> tried and it didn't work
-    icon.addEventListener(input, (e) => {
-      e.preventDefault()    // Prevent 'click' to be triggered as well when tapping on the icon using touch screens
-      if (navbar.id === "topNav") {
-        navbar.id += "_responsive";   // Change id of element to 'topNav_responsive'
-      } else {
-        navbar.id = "topNav";   // If id name is already 'topNav_responsive', change it back to 'topNav'
-      }
-    }, false);
-  });
+  function toggleMenu() {
 
-}, false);
+    // ["click", "touchstart"].forEach( input => {   // Could use touchstart only ; supposed to work on touch and click >> tried and it didn't work
+      // icon.addEventListener("click", (e) => {
+      //   e.preventDefault()    // Prevent 'click' to be triggered as well when tapping on the icon using touch screens
+
+        if (navbar.id === "topNav") {
+          navbar.id += "_responsive";   // Change id of element to 'topNav_responsive'
+        } else {
+          navbar.id = "topNav";   // If id name is already 'topNav_responsive', change it back to 'topNav'
+        }
+      // }, false);
+    // });
+  }
+
+// }, false);
 
 // icon.addEventListener("click", (e) => {
 //   if (navbar.id === "topNav") {
