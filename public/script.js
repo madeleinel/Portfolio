6850cc94fa2c8801181fdd48da2887416624e810
madeleinel <madeleinelinder@hotmail.com>
2017-05-21 17:29:03 +0100

    Continuing to work on the Portfolio page redesign

--- public/script.js
+++ public/script.js
@@ -26,23 +26,20 @@ function displayPopup(buttonId) {
   let popupId = "popup_" + id[1];
   let currentPopup = document.getElementById(popupId);
   let sectionId = "section_" + id[1];
-  // let currentSection = document.getElementById(sectionId);
   let currentButton = document.querySelector("#" + sectionId + " span:last-child");
-  console.log(id);
-  console.log(popupId);
-  console.log(currentPopup);
-  console.log(sectionId);
-  console.log(currentButton);
+  let currentButtonText = document.querySelector("#" + sectionId + " span:last-child button");
 
   // To toggle the popup: Hide if it's visible, and make visible if it's hidden
   // Also update the background colour of the clicked button when the popup is visible, to indicate that it is currently "active"
   if (currentPopup.style.display == "block") {
     currentPopup.style.display = "none";
-    // currentButton.style.backgroundColor = "#92626F";
-    currentButton.style.backgroundImage = "url('images/chalkboard.jpg')";
+    // currentButton.style.backgroundImage = "url('images/chalkboard.jpg')";
+    currentButton.style.borderColor = "#333";
+    currentButtonText.textContent = "Find out more ⇓";
   } else {
     currentPopup.style.display = "block";
-    // currentButton.style.backgroundColor = "#333";
-    currentButton.style.backgroundImage = "url('images/redChalkboard.png')";
+    // currentButton.style.backgroundImage = "url('images/redChalkboard.png')";
+    currentButton.style.borderColor = "#92626F";
+    currentButtonText.textContent = "Find out more ⇑";
   }
 }
