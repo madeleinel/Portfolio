014b6089d872cde7fb350400bdf243471cc17093
madeleinel <madeleinelinder@hotmail.com>
2017-05-22 18:21:18 +0100

    Working on solving issues with onClick functions on phone devices with touchscreens

--- public/script.js
+++ public/script.js
@@ -3,6 +3,7 @@
 function enterSite() {
   window.location.href = "/about";
 }
+document.getElementById("landingPage").addEventListener("click", enterSite);
 
 // Set up dropdown menu for small screens
 const icon = document.getElementById("navToggleIcon");
@@ -20,10 +21,10 @@ function togglePopup(buttonId) {
   // buttonId = The class name of the button that was clicked
   // Set the popup ID to be "popup_" followed by the second half of the buttonId (ie the id number of the section)
   // This will be used to display the popup that's within the same section as the clicked button
-  let id = buttonId.split("_");
-  let popupId = "popup_" + id[1];
+  let idTags = buttonId.split("_");
+  let popupId = "popup_" + idTags[1];
   let currentPopup = document.getElementById(popupId);
-  let sectionId = "section_" + id[1];
+  let sectionId = "section_" + idTags[1];
   let currentButton = document.querySelector("#" + sectionId + " span:last-child");
   let currentButtonText = document.querySelector("#" + sectionId + " span:last-child button");
 
@@ -31,13 +32,11 @@ function togglePopup(buttonId) {
   // Also update the background colour of the clicked button when the popup is visible, to indicate that it is currently "active"
   if (currentPopup.style.display == "block") {
     currentPopup.style.display = "none";
-    // currentButton.style.backgroundImage = "url('images/chalkboard.jpg')";
     currentButton.style.borderColor = "#333";
     currentButtonText.textContent = "Find out more ⇓";
     window.location.href = ("/portfolio\#" + sectionId);  // Makes the window jump back to the top of the section the user was just looking at > to avoid the screen seemingly jumping to a later section, and skipping work examples, when closing a popup section
   } else {
     currentPopup.style.display = "block";
-    // currentButton.style.backgroundImage = "url('images/redChalkboard.png')";
     currentButton.style.borderColor = "#92626F";
     currentButtonText.textContent = "Find out more ⇑";
   }
