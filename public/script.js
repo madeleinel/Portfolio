2508939d0a27c02928c69e338e770e692e166d7f
madeleinel <madeleinelinder@hotmail.com>
2017-05-23 15:39:23 +0100

    Solving issues on small and touchscreen devices: rewriting js code to make the click functions work on touch screens, updating page layouts and sizes to look good on small screens, making the footer 100vw to avoid overflow-x issues (scrolling side-to-side), updating the styling on the landing page, to fix the scaling issue that followed from adding a link to the #landingPage element, making the Portfolio page buttons smaller on small screens, making the contact icon row wider on small screen, to ensure they are still round. Updating reset.css to remove default styling of button elements.

--- public/script.js
+++ public/script.js
@@ -1,32 +1,26 @@
-// Go to path "/about" when click on landing page svg
-// Have to set it up as a function, as adding a link within the svg did not function on touchscreen
-function enterSite() {
-  window.location.href = "/about";
-}
-document.getElementById("landingPage").addEventListener("click", enterSite);
-
 // Set up dropdown menu for small screens
-const icon = document.getElementById("navToggleIcon");
-const navbar = document.getElementById("topNav");
-function toggleMenu() {
+function toggleMenu(e) {
+  e.preventDefault(); // Prevent the function from executing twice on touchscreens due to double calls to the eventListener function ("click" and "touch")
+  const navbar = document.getElementById("topNav");
   if (navbar.className === "navBar") {
     navbar.className += " responsive";   // Change the class of element to 'navBar responsive'
   } else {
     navbar.className = "navBar";   // If the class is already 'navBar responsive', change it back to 'navBar'
   }
 }
+document.getElementById("navToggleIcon").addEventListener("click", toggleMenu, false);
 
 // Set up section toggling within the Portfolio page
 function togglePopup(buttonId) {
   // buttonId = The class name of the button that was clicked
   // Set the popup ID to be "popup_" followed by the second half of the buttonId (ie the id number of the section)
   // This will be used to display the popup that's within the same section as the clicked button
-  let idTags = buttonId.split("_");
-  let popupId = "popup_" + idTags[1];
-  let currentPopup = document.getElementById(popupId);
-  let sectionId = "section_" + idTags[1];
-  let currentButton = document.querySelector("#" + sectionId + " span:last-child");
-  let currentButtonText = document.querySelector("#" + sectionId + " span:last-child button");
+  const idTags = buttonId.split("_");
+  const popupId = "popup_" + idTags[1];
+  const currentPopup = document.getElementById(popupId);
+  const sectionId = "section_" + idTags[1];
+  const currentButton = document.querySelector("#" + sectionId + " span:last-child");
+  const currentButtonText = document.querySelector("#" + sectionId + " span:last-child button");
 
   // To toggle the popup: Hide if it's visible, and make visible if it's hidden
   // Also update the background colour of the clicked button when the popup is visible, to indicate that it is currently "active"
@@ -41,3 +35,11 @@ function togglePopup(buttonId) {
     currentButtonText.textContent = "Find out more ⇑";
   }
 }
+
+// Replaced with a link over the landing page:
+// Go to path "/about" when click on landing page svg
+// Have to set it up as a function, as adding a link within the svg did not function on touchscreen
+// function enterSite() {
+//   window.location.href = "/about";
+// }
+// document.getElementById("landingPage").addEventListener("click", enterSite);
