72516d07c96a7c4714a9c4268b3bf153e352eb2b
madeleinel <madeleinelinder@hotmail.com>
2017-09-15 23:58:12 +0100

    Continuing to work on the redesign of the site; creating a new Main Projects section, and restyling the Just for Fun section slightly.

--- routes.js
+++ routes.js
@@ -4,15 +4,11 @@ module.exports = (app) => {
 
   // Define the home page route
   app.get('/', (req, res) => {
-    res.render('index.pug');
+    res.render('layout.pug');
   });
 
   // Define the portfolio page route
   app.get('/portfolio', (req, res) => {
     res.render('portfolio.pug');
-  });
-
-  app.get('/templating', (req, res) => {
-    res.render('layout.pug');
   });
 }
