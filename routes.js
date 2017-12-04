3d8ca0a55163681620339027f86e4ec32c16056b
madeleinel <madeleinelinder@hotmail.com>
2017-12-04 21:47:49 +0000

    Creating a temporary landing page for WIP site

--- routes.js
+++ routes.js
@@ -1,14 +1,7 @@
 'use strict';
 
 module.exports = (app) => {
-
-  // Define the home page route
   app.get('/', (req, res) => {
     res.render('layout.pug');
-  });
-
-  // Define the portfolio page route
-  app.get('/portfolio', (req, res) => {
-    res.render('portfolio.pug');
   });
 }
